eeedac96d3999fb35c2b92d1abbf0c9eaff5d189
Gonzalo <105743115+CiberAfro247@users.noreply.github.com>
2022-08-29 10:27:09 -0300

    Update signup.php

--- php/signup.php
+++ php/signup.php
@@ -4,12 +4,13 @@ require 'database.php';
 $message = '';
 
 if (!empty($_POST['email']) && !empty($_POST['password'])) {
-  $sql = "INSERT INTO usuarios (nombre, apellido, clave, telefono, mail, Administrador) VALUES (:nombrereg, :apellidoreg, :password, :numerotel, :email, 0)";
+  $sql = "INSERT INTO usuarios (nombre, apellido, clave, telefono, mail, Administrador, Grado) VALUES (:nombrereg, :apellidoreg, :password, :numerotel, :email, 0, :grado)";
   $stmt = $conn->prepare($sql);
   $stmt->bindParam(':nombrereg', $_POST['nombrereg']);
   $stmt->bindParam(':apellidoreg', $_POST['apellidoreg']);
   $stmt->bindParam(':numerotel', $_POST['numerotel']);
   $stmt->bindParam(':email', $_POST['email']);
+  $stmt->bindParam(':grado', $_POST['grado']);
   $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
   $stmt->bindParam(':password', $password);
   
@@ -18,6 +19,7 @@ if (!empty($_POST['email']) && !empty($_POST['password'])) {
     header('Location: Inicio_usuario_comun.html');
   } else {
     $message = 'Lo lamentamos, no pudimos crear su usuario';
+    header('Location: signup.php');
   }
 }
 ?>
