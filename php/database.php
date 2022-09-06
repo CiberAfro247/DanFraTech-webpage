34218833955b477a2c20db1d6af2e0e515e9c0fe
Fran9804 <105743175+Fran9804@users.noreply.github.com>
2022-09-06 13:32:07 -0300

    Update database.php

--- php/database.php
+++ php/database.php
@@ -1,12 +1,15 @@
 <?php
 
 $server = 'localhost';
-$username = 'root';
-$password = '';
+$username = 'administracion';
+$password = 'admin';
 $database = 'proyecto';
+$usernameu = 'usuario';
+$passwordu = 'user';
 
 try {
   $conn = new PDO("mysql:host=$server;dbname=$database;", $username, $password);
+  $connu = new PDO("mysql:host=$server;dbname=$database;", $usernameu, $passwordu);
 } catch (PDOException $e) {
   die('Connection Failed: ' . $e->getMessage());
 }
