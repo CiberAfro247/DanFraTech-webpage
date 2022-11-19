1abff060d85e240d26958b612b3bffc18c4c0375
Fran9804 <105743175+Fran9804@users.noreply.github.com>
2022-11-18 21:51:54 -0300

    Update eliminar_usuario.php

--- php/eliminar_usuario.php
+++ php/eliminar_usuario.php
@@ -1,18 +1,23 @@
 <?php
 	session_start();
 
-	if (isset($_SESSION['user_id'])) {
-		header('Location: /DanFraTech-webpage-main');
-	}
 	require 'database.php';
 
 	if (!empty($_POST['email'])) {
-		$query_eliminar = "DELETE FROM usuarios WHERE Mail = :email";
+		$query_eliminar = "
+		DELETE FROM contactos_usuario WHERE contactos_usuario.ID_Usuario_1 = (SELECT usuarios.ID_Usuario FROM usuarios WHERE usuarios.Mail = :email);
+		DELETE FROM contactos_usuario WHERE contactos_usuario.ID_Usuario_2 = (SELECT usuarios.ID_Usuario FROM usuarios WHERE usuarios.Mail = :email);
+		DELETE FROM realizan WHERE realizan.ID_Usuario = (SELECT usuarios.ID_Usuario FROM usuarios WHERE usuarios.Mail = :email);
+		DELETE FROM prestamos WHERE prestamos.ID_Usuario = (SELECT usuarios.ID_Usuario FROM usuarios WHERE usuarios.Mail = :email);
+		DELETE FROM reciben WHERE reciben.ID_Usuario = (SELECT usuarios.ID_Usuario FROM usuarios WHERE usuarios.Mail = :email);
+		DELETE FROM forman WHERE forman.ID_Usuario = (SELECT usuarios.ID_Usuario FROM usuarios WHERE usuarios.Mail = :email);
+		DELETE FROM historial_usuarios WHERE historial_usuarios.ID_Usuario = (SELECT usuarios.ID_Usuario FROM usuarios WHERE usuarios.Mail = :email);
+		DELETE FROM usuarios WHERE usuarios.ID_Usuario = (SELECT usuarios.ID_Usuario FROM usuarios WHERE usuarios.Mail = :email);";
 		$records = $conn->prepare($query_eliminar);
 		$records->bindParam(':email', $_POST['email']);
 		
 		if ($records->execute()) {
-			header('Location: eliminar_usuario.html');
+			header('Location: ../html/eliminar_usuario.html');
 		}
 	}
 	
