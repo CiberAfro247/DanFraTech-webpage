a7b05be40db674a0d1922788da76ac17b28562fe
Fran9804 <105743175+Fran9804@users.noreply.github.com>
2022-11-18 21:17:15 -0300

    Update club_de_lectura_administrador.php

--- php/club_de_lectura_administrador.php
+++ php/club_de_lectura_administrador.php
@@ -48,11 +48,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<link rel="icon" href="Imagenes/Bookneith.png">
+	<link rel="icon" href="../Imagenes/Bookneith.png">
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
-  	<link rel="stylesheet" type="text/css" href="style.css">
+  	<link rel="stylesheet" type="text/css" href="../css/style.css">
   	<script src="js/bootstrap.min.js"></script>
   	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   	<script src="https://kit.fontawesome.com/4c9b3a5a5f.js" crossorigin="anonymous"></script>
